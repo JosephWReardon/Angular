cca5468a8d0d07c27d21c239fa4c1044c4644a16
Joseph Reardon <josephwreardon@gmail.com>
2022-11-15 13:51:24 -0600

    Added demo app

---
+++ angularPractice/src/app/demo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'demo',
+    template: '<h2>The famous philosipher proposed that a man was a featherless biped</h2>'
+})
+
+export class DemoComponent {
+    title = "Application"
+}
